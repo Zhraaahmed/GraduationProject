fee401e4c82ee0c8b7bc124b9435a3b6bc0f2507
Al-zhraa Ahmed <90254544+Zhraaahmed@users.noreply.github.com>
2023-02-24 18:39:47 +0200

    Add files via upload

---
+++ first_page.dart
@@ -0,0 +1,94 @@
+import 'package:first_day_flutter/modules/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(28.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsetsDirectional.all(40
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Image(image: NetworkImage('https://img.freepik.com/vrije-vector/gezonde-mensen-met-verschillende-pictogrammen_53876-43069.jpg?w=2000'),
+
+                  ),
+                ),
+              ), SizedBox(height: 30,),
+              Text('My Behavior' ,style: TextStyle(color: Colors.blue,fontSize: 30.0,fontWeight: FontWeight.bold),),
+              SizedBox(height: 30,),
+              Text('Welcome to My Behavior!' ,style: TextStyle(fontSize:20)),  SizedBox(height: 75,),
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(20)
+          ),
+
+          width: double.infinity,
+          child: MaterialButton(onPressed:(){
+            print('Sign in ');
+
+          } ,child: Text('Sign Up',style: TextStyle(color: Colors.white),),),
+        ),SizedBox(height: 15,),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: Colors.blue
+                  )
+                ),
+
+                width: double.infinity,
+                child: MaterialButton(onPressed:(){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+
+
+                } ,child: Text('Sign in',style: TextStyle(color: Colors.blue),),),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:first_day_flutter/layout/home_layout.dart';
+import 'package:first_day_flutter/modules/counter/Counter_screen.dart';
+import 'package:first_day_flutter/modules/login/first_page.dart';
+import 'package:first_day_flutter/modules/users/Users_screen.dart';
+import 'package:first_day_flutter/modules/bmi_result/bmi_result_screen.dart';
+import'package:first_day_flutter/modules/bmi/bmi_screen.dart';
+import 'package:first_day_flutter/modules/home/home_screen.dart';
+import 'package:first_day_flutter/modules/login/login_screen.dart';
+import 'package:first_day_flutter/modules/messenger/messenger_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:FirstPage(),
+    );
+
+  }
+}
+
