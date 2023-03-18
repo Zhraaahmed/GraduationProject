f1b3518ac107310c860300d41bdd5ba77d89dd38
Al-zhraa Ahmed <90254544+Zhraaahmed@users.noreply.github.com>
2023-03-19 01:51:33 +0200

    Add files via upload

---
+++ Account_type.dart
@@ -0,0 +1,67 @@
+import 'package:first_day_flutter/doctorprofile.dart';
+import 'package:first_day_flutter/modules/login/first_page.dart';
+import 'package:flutter/material.dart';
+
+import 'First_doc.dart';
+import 'modules/login/login_screen.dart';
+import 'modules/login/signup_page.dart';
+
+class Account extends StatelessWidget {
+  const Account({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Center(child: Text('Choose Account Type',style: TextStyle(color:Colors.blue,fontSize: 30.0,fontWeight: FontWeight.bold),)),
+            ), SizedBox(height: 50,),
+            Center(
+              child: Row(
+                children: [
+                  InkWell( onTap:(){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FirstPage()),
+                    );},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(205),
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage('Images/pngtre.jpg' )
+                        ),
+                      ),width: 170,height: 350,),
+                  ),  SizedBox(width:12,),
+                  InkWell(onTap:(){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => FirstDoc()),
+                      );},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(205),
+                          image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage('Images/30.jpg' ))
+                          ), width: 130,height:350,),
+                  )
+                ],
+              ),),
+            Row(
+              children: [ SizedBox(width: 50,),
+                Text('User',style: TextStyle(color:Colors.blue,fontSize: 30.0,fontWeight: FontWeight.bold)), SizedBox(width: 80,),
+                Text('Doctor',style: TextStyle(color:Colors.blue,fontSize: 30.0,fontWeight: FontWeight.bold)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+}
+}
---
+++ sign_doc.dart
@@ -0,0 +1,163 @@
+import 'package:first_day_flutter/doctorprofile.dart';
+import 'package:first_day_flutter/log_doc.dart';
+import 'package:flutter/material.dart';
+
+class SignDoc extends StatefulWidget {
+  const SignDoc({Key? key}) : super(key: key);
+
+  @override
+  State<SignDoc> createState() => _SignDocState();
+}
+
+class _SignDocState extends State<SignDoc> {
+  var emailController=TextEditingController();
+
+  var passwordController=TextEditingController();
+
+  bool isPassword=true;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsetsDirectional.all(40
+                  ),
+                ),
+                Center(child: Text('Sign Up' ,style: TextStyle(color:Colors.blue,fontSize: 30.0,fontWeight: FontWeight.bold),)),
+                SizedBox(height: 20,),
+                Center(child: Text('Create your Account' ,style: TextStyle(fontSize: 20.0))),
+                SizedBox(height: 20,),
+                Text('Email',style: TextStyle(fontWeight: FontWeight.bold),),
+                SizedBox(height: 20,),
+                TextFormField(
+                  autovalidateMode:
+                  AutovalidateMode.onUserInteraction,
+                  controller: emailController,
+                  onFieldSubmitted: (value){
+                    print(value);
+                  },onChanged: (value){
+                  print(value);
+                },
+                  validator: (String? value) {
+                    if (value!.isEmpty) return "E-mail must be not empty";
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Email Address',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email)
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Text('Password',style: TextStyle(fontWeight: FontWeight.bold),),
+                SizedBox(height: 20,),
+                TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    onFieldSubmitted: (value){
+                      print(value);
+                    },onChanged: (value){
+                  print(value);
+                },
+                    validator: (String? value) {
+                      if (value!.isEmpty) return "password must be not empty";
+                    },
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconData!=null?IconButton(onPressed: (){
+                        setState(() {
+                          isPassword=!isPassword;
+                        });
+                      }, icon: Icon(Icons.remove_red_eye)):null,
+
+                    )
+                ), SizedBox(height: 20,),
+                SizedBox(height: 5,),
+                Text('Confirm Password',style: TextStyle(fontWeight: FontWeight.bold),),
+                SizedBox(height: 15,),
+                TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    onFieldSubmitted: (value){
+                      print(value);
+                    },onChanged: (value){
+                  print(value);
+                },
+                    validator: (String? value) {
+                      if (value!.isEmpty) return "You Must Conirm Password";
+                    },
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconData!=null?IconButton(onPressed: (){
+                        setState(() {
+                          isPassword=!isPassword;
+                        });
+                      }, icon: Icon(Icons.remove_red_eye)):null,
+
+                    )
+                ), SizedBox(height: 20,),
+                SizedBox(height: 5,),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+
+                  width: double.infinity,
+                  child: MaterialButton(onPressed:(){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DocProf()),
+                    );
+                    if (_formKey.currentState!.validate()) {
+                      print(emailController.text);
+                      print(passwordController.text);
+                    }
+                    print('Sign Up');
+
+                  } ,child: Text('Sign Up',style: TextStyle(color: Colors.white),),),
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(alignment: Alignment.center,
+                        child: Text('Already Have An Account?',style: TextStyle(color: Colors.black),)), SizedBox(height: 15,),
+                    Container(alignment: Alignment.center,
+                        child: TextButton(onPressed:(){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => LoginDoc()),
+                          );
+                        }
+                            , child: Text('Login'))),
+                  ],
+                )
+              ],
+
+
+
+
+
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
