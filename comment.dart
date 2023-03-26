34a837655ed877227d36d8425b939c97c4e0b430
Al-zhraa Ahmed <90254544+Zhraaahmed@users.noreply.github.com>
2023-03-26 16:51:53 +0200

    Add files via upload

---
+++ comment.dart
@@ -0,0 +1,58 @@
+import 'package:first_day_flutter/Questions.dart';
+import 'package:flutter/material.dart';
+
+class CommentPage extends StatelessWidget {
+  const CommentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body:
+    Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 190,
+          ),
+          Card(
+            elevation: 5,
+            child: SizedBox(
+              width: 750,
+              height: 150,
+              child: ListTile(
+                leading:  CircleAvatar(
+                  radius: 25.0,
+                  backgroundImage: NetworkImage(
+                      'https://thevideoink.com/wp-content/uploads/2021/11/wysiwyg-uploads_1569586526901-doctor.jpg'),
+                ),
+                title: Text('Dr. MOHAMED'),
+                subtitle: TextFormField(),
+
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+               color: Colors.blue
+            ),
+
+            width: double.infinity,
+            child: MaterialButton(onPressed:(){
+              Navigator.pop(
+                context,
+                MaterialPageRoute(builder: (context) => Questions()),
+              );
+
+
+            } ,child: Text('Comment',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),),
+          ),
+        ],
+      ),
+    ),
+    );
+  }
+}
